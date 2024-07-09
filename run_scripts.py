79920dfb30006123bd15c46bba7ff4df56993efd
benedettagambosi <73115526+benedettagambosi@users.noreply.github.com>
2024-07-09 11:55:41 +0200

    Update run_scripts.py

--- run_scripts.py
+++ run_scripts.py
@@ -1,11 +1,10 @@
 import subprocess
 
 trials = 5
-exp = 1
+exp = 1 # indexing this list -> ['active', 'EBCC']
 for idx in range(trials):
-    for mod in [1]: #range(3):
-        # for dopa in [0,3,4]: #range(5):
-        for dopa in range(5):
+    for mod in range(3): # indexing this list -> ['external_dopa', 'internal_dopa', 'both_dopa'] external: only bg, internal only cerebellum
+        for dopa in range(5): # indexing this list -> [0.,-0.1,-0.2,-0.4,-0.8]
             program = [f'./main.py {str(idx + 1)} {str(mod)} {str(exp)} {str(dopa)}']
 
             subprocess.call(program, shell=True)    # , capture_output=True)
