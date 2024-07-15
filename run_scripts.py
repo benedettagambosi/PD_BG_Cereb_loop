f4f8a91b27dae063975f87cdd7dbe6d3db261954
benedettagambosi <73115526+benedettagambosi@users.noreply.github.com>
2024-07-15 12:03:47 +0200

    Update run_scripts.py
    simualtes only the necessary dopa level in EBCC

--- run_scripts.py
+++ run_scripts.py
@@ -3,9 +3,15 @@ import sys
 
 trials = 5
 exp =  int(sys.argv[1]) # indexing this list -> ['active', 'EBCC']
+
+if exp:
+    dopa = [0,3,4] # -> simulates only physio, middle severity and worse pathological case
+else:
+    dopa = range(5)
+    
 for idx in range(trials):
     for mod in range(3): # indexing this list -> ['external_dopa', 'internal_dopa', 'both_dopa'] external: only bg, internal only cerebellum
-        for dopa in range(5): # indexing this list -> [0.,-0.1,-0.2,-0.4,-0.8]
+        for dopa in dopa: # indexing this list -> [0.,-0.1,-0.2,-0.4,-0.8]
             program = [f'./main.py {str(idx + 1)} {str(mod)} {str(exp)} {str(dopa)}']
 
             subprocess.call(program, shell=True)    # , capture_output=True)
