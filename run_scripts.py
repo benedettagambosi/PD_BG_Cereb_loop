0b86f40f20b3d139421fe28fb4900fd4d9bad039
benedettagambosi <benedetta.gambosi@polimi.it>
2024-07-15 12:34:41 +0200

    uncommented run

--- run_scripts.py
+++ run_scripts.py
@@ -14,5 +14,5 @@ for idx in range(trials):
         for dopa in dopa_lvl: # indexing this list -> [0.,-0.1,-0.2,-0.4,-0.8]
             program = [f'./main.py {str(idx + 1)} {str(mod)} {str(exp)} {str(dopa)}']
             
-            #subprocess.call(program, shell=True)    # , capture_output=True)
+            subprocess.call(program, shell=True)    # , capture_output=True)
             print("Finished:" + program[0])
