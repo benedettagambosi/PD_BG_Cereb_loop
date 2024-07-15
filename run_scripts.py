21eb20fd7552bfff790664ea225958e25729f731
benedettagambosi <benedetta.gambosi@polimi.it>
2024-07-15 12:09:31 +0200

    dopa 0, 0.4, 0.8, for EBCC

--- run_scripts.py
+++ run_scripts.py
@@ -5,14 +5,14 @@ trials = 5
 exp =  int(sys.argv[1]) # indexing this list -> ['active', 'EBCC']
 
 if exp:
-    dopa = [0,3,4] # -> simulates only physio, middle severity and worse pathological case
+    dopa_lvl = [0,3,4] # -> simulates only physio, middle severity and worse pathological case
 else:
-    dopa = range(5)
+    dopa_lvl = range(5)
     
 for idx in range(trials):
     for mod in range(3): # indexing this list -> ['external_dopa', 'internal_dopa', 'both_dopa'] external: only bg, internal only cerebellum
-        for dopa in dopa: # indexing this list -> [0.,-0.1,-0.2,-0.4,-0.8]
+        for dopa in dopa_lvl: # indexing this list -> [0.,-0.1,-0.2,-0.4,-0.8]
             program = [f'./main.py {str(idx + 1)} {str(mod)} {str(exp)} {str(dopa)}']
-
-            subprocess.call(program, shell=True)    # , capture_output=True)
+            
+            #subprocess.call(program, shell=True)    # , capture_output=True)
             print("Finished:" + program[0])
